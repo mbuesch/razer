4c004416c1f21dd5d857fc5d58e5fbc5677ca51a
Michael Buesch <m@bues.ch>
2014-04-21 21:15:59 +0200

    Move razerd binary from sbin to bin
    This also increments the interface version number to make sure we don't
    talk to old daemons.
    
    Signed-off-by: Michael Buesch <m@bues.ch>

--- ui/pyrazer.py
+++ ui/pyrazer.py
@@ -156,7 +156,7 @@ class Razer(object):
 	SOCKET_PATH	= "/var/run/razerd/socket"
 	PRIVSOCKET_PATH	= "/var/run/razerd/socket.privileged"
 
-	INTERFACE_REVISION = 4
+	INTERFACE_REVISION = 5
 
 	COMMAND_MAX_SIZE = 512
 	COMMAND_HDR_SIZE = 1
