fe6da6f14a33198cff22683212a386537c50d698
Michael Buesch <m@bues.ch>
2014-04-26 11:35:59 +0200

    Move PYRAZER_SETUP_PY to __init__.py
    Signed-off-by: Michael Buesch <m@bues.ch>

--- ui/pyrazer/__init__.py
+++ ui/pyrazer/__init__.py
@@ -1,1 +1,6 @@
+# This variable indicates that the module was installed
+# with setup.py. This is used to detect older versions of
+# pyrazer that were not installed with setup.py into the system.
+PYRAZER_SETUP_PY = True
+
 from pyrazer.main import *
--- ui/pyrazer/main.py
+++ ui/pyrazer/main.py
@@ -27,12 +27,6 @@ import struct
 RAZER_VERSION	= "0.25"
 
 
-# This variable indicates that the module was installed
-# with setup.py. This is used to detect older versions of
-# pyrazer that were not installed with setup.py into the system.
-PYRAZER_SETUP_PY = True
-
-
 class RazerEx(Exception):
 	"Exception thrown by pyrazer code."
 
