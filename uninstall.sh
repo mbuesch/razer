142b5da6a9e29d8902498cf897bf83fab7fb522d
polyzen <polycitizen@gmail.com>
2014-07-11 18:32:41 -0400

    Use pkg-config to set udev and systemd dirs

--- uninstall.sh
+++ uninstall.sh
@@ -65,8 +65,9 @@ uninstall_global()
 {
 	for f in /etc/razer.conf /etc/init.d/razerd /etc/rc*.d/*razerd\
 		 /etc/pm/sleep.d/*-razer /etc/udev/rules.d/*-razer-udev.rules\
+		 /lib/udev/rules.d/*-razer-udev.rules\
 		 /etc/systemd/system/razerd.service\
-		 /usr/lib/systemd/system/razerd.service; do
+		 /lib/systemd/system/razerd.service; do
 
 		local path="$f"
 		[ -e "$path" -o -h "$path" ] || continue
