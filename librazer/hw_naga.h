d2f9b508817b824a8bc3dd7cbf2e4a3531b67078
Corentin Jabot <corentinjabot@gmail.com>
2015-10-06 15:03:47 +0200

    Add support for a newer naga hex

--- librazer/hw_naga.h
+++ librazer/hw_naga.h
@@ -9,6 +9,7 @@
 #define RAZER_NAGA_PID_2012     0x002e
 #define RAZER_NAGA_PID_HEX      0x0036
 #define RAZER_NAGA_PID_2014     0x0040
+#define RAZER_NAGA_PID_HEX_2014 0x0041
 
 
 int razer_naga_init(struct razer_mouse *m,
