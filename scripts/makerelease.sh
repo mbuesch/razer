1fe4956a006a8483ae07c1f638073afe8476d377
Michael Buesch <m@bues.ch>
2024-05-30 11:30:04 +0200

    Update release scripts and remove Debian build scripts
    The razercfg Debian build scripts are obsolete and don't work correctly anymore.
    Please use the build scripts from the Debian repository instead.

--- scripts/makerelease.sh
+++ scripts/makerelease.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-srcdir="$(dirname "$0")"
-[ "$(echo "$srcdir" | cut -c1)" = '/' ] || srcdir="$PWD/$srcdir"
-
+srcdir="$(realpath "$0" | xargs dirname)"
 srcdir="$srcdir/.."
 
-die() { echo "$*"; exit 1; }
-
 # Import the makerelease.lib
-# http://bues.ch/gitweb?p=misc.git;a=blob_plain;f=makerelease.lib;hb=HEAD
+# https://bues.ch/cgit/misc.git/tree/makerelease.lib
+die() { echo "$*"; exit 1; }
 for path in $(echo "$PATH" | tr ':' ' '); do
 	[ -f "$MAKERELEASE_LIB" ] && break
 	MAKERELEASE_LIB="$path/makerelease.lib"
